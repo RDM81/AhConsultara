c82b4b77b7f21a3e2d57ba90a2f2f1c45ba6625b
probando cards principales <roberto_arg81@hotmail.com>
2022-05-13 16:17:31 -0300

    Pagina en Construccion

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+//----LOADER-----//
+
+window.addEventListener('load', () => {
+    const contenedor_loader = document.querySelector('.contenedor_loader');
+    
+    contenedor_loader.style.visibility = 'hidden';
+    contenedor_loader.style.opacity = 0;
+
+})
